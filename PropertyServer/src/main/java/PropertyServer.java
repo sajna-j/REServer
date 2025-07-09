1a10500d8296b5d3285fdfdaa66f70fbe74bbd79
maxbolz <maxfbolz@gmail.com>
2025-07-09 11:22:12 +1000

    property server :D

--- PropertyServer/src/main/java/PropertyServer.java
+++ PropertyServer/src/main/java/PropertyServer.java
@@ -21,7 +21,7 @@ public class PropertyServer {
             config.registerPlugin(new ReDocPlugin(uiConfig -> uiConfig.setUiPath("/docs/redoc")));
 
             config.router.apiBuilder(() -> {
-                get("/", ctx -> ctx.result("Real Estate server is running"));
+                get("/", ctx -> ctx.result("Property server is running"));
                 get("/sales/{saleID}", ctx -> salesHandler.getSaleByID(ctx, ctx.pathParam("saleID")));
                 get("/sales", salesHandler::getAllSales);
                 post("/sales", salesHandler::createSale);
@@ -30,6 +30,6 @@ public class PropertyServer {
                 get("/sales/price_per_square_meter/high", salesHandler::highPricePerSquareMeter);
                 get("/sales/price_per_square_meter/low", salesHandler::lowPricePerSquareMeter);
             });
-        }).start(7070);
+        }).start(7071);
     }
 }
